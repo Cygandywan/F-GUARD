45683d2bffba2116384d40f4cf18ff6cb1056be5
Cygandywan <56272388+Cygandywan@users.noreply.github.com>
2019-10-13 22:23:13 +0200

    Update my_bot.js

--- my_bot.js
+++ my_bot.js
@@ -1,14 +1,25 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
 
 client.on('ready', () => {
-    // Set bot status to: "Playing with JavaScript"
-    client.user.setActivity("with JavaScript")
 
-    // Alternatively, you can set the activity to any of the following:
-    // PLAYING, STREAMING, LISTENING, WATCHING
-    // For example:
-    // client.user.setActivity("TV", {type: "WATCHING"})
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
 });
 
  
